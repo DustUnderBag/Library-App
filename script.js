9e42a8553c2a3368671b4572f2a4aa7a054d2a5c
DustUnderBag <usamary55@gmail.com>
2024-07-19 00:07:46 -0400

    Implement creating card from new book object
    The created cards, as well as its readToggle delete button, will be assigned
    a data-index which is to associate the the card to its own corresponding
    book.

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
-const newBook_btn = document.getElementById('add-book');
-const modal = document.querySelector('dialog.book-form');
+//Modal input elements
+const btn_newBook = document.querySelector("button#add-book");
+const modal = document.querySelector('dialog.book-modal');
+
+const input_title = document.querySelector("input#title");
+const input_author = document.querySelector("input#author");
+const input_pages = document.querySelector("input#pages");
+const text_inputs = [input_title, input_author, input_pages];
+
+const input_isRead = document.querySelector("input#isRead");
+const btn_submit = document.querySelector("button[type='submit']");
+
+//Cards
+const cards_container = document.querySelector(".cards-container");
 
 const myLibrary = [];
 
@@ -20,13 +32,7 @@ function addBookToLibrary(obj) {
     myLibrary.push(obj);
 }
 
-let book1 = new Book("Pride and Prejudice", "Jane Austen", 363, true);
-let book2 = new Book("The Little Prince", "Antoine de Saint-Exupéry", 102, false);
-
-addBookToLibrary(book1);
-addBookToLibrary(book2);
-
-function updateLibraryCards() {
+function displayLibrary() {
     for(let i = 0; i < myLibrary.length; i++) {
         let id = 'book' + (i + 1);
         console.log(id);
@@ -35,15 +41,79 @@ function updateLibraryCards() {
         const pages = document.querySelector(`#${id} .pages`);
         console.log(title, author, pages);
 
-        title.textContent += myLibrary[i].title;
-        author.textContent += myLibrary[i].author;
-        pages.textContent += myLibrary[i].pages + " total pages";
+        title.textContent = myLibrary[i].title;
+        author.textContent = myLibrary[i].author;
+        pages.textContent = myLibrary[i].pages + " total pages";
     }
 }
 
-newBook_btn.addEventListener('click', () => {
+/*
+let book1 = new Book("Pride and Prejudice", "Jane Austen", 363, true);
+let book2 = new Book("The Little Prince", "Antoine de Saint-Exupéry", 102, false);
+*/
+
+btn_newBook.addEventListener('click', () => {
     modal.showModal();
 });
 
-updateLibraryCards();
 
+function createNewBook() {
+    let title = input_title.value;
+    let author = input_author.value;
+    let pages = input_pages.value;
+    let isRead = (input_isRead.checked) ? true : false;
+    
+    let temp_obj = new Book(title, author, pages, isRead);
+    console.log(temp_obj);
+    return temp_obj;
+}
+
+function clearFormInputs() {
+    for(input of text_inputs) {
+        input.value = "";
+    }
+    input_isRead.checked = false;
+}
+
+function createCard(book, index) {
+    const card_DOM = document.createElement('div');
+    card_DOM.classList.add('card');
+    card_DOM.setAttribute('data-index', index);
+
+    const title_DOM = document.createElement('h2');
+    title_DOM.textContent = book.title;
+
+    const author_DOM = document.createElement('p');
+    author_DOM.textContent = book.author;
+
+    const pages_DOM = document.createElement('p');
+    pages_DOM.textContent = book.pages;
+
+    const read_DOM = document.createElement('input');
+    read_DOM.setAttribute('type', 'checkbox');
+    read_DOM.setAttribute('data-index', index);
+    read_DOM.checked = (book.isRead) ? true : false;
+
+    const delete_Btn = document.createElement('button');
+    delete_Btn.textContent = "X";
+    delete_Btn.classList.add('delete_btn');
+    delete_Btn.setAttribute('data-index', index);
+
+    cards_container.appendChild(card_DOM);
+    card_DOM.appendChild(title_DOM);
+    card_DOM.appendChild(author_DOM);
+    card_DOM.appendChild(pages_DOM);
+    card_DOM.appendChild(read_DOM);
+    card_DOM.appendChild(delete_Btn);
+}
+
+btn_submit.addEventListener('click', e => {
+    addBookToLibrary(createNewBook());
+    let index = myLibrary.length - 1;
+    createCard(myLibrary[index], index);
+
+
+    clearFormInputs();
+    modal.close();
+    e.preventDefault();
+})
