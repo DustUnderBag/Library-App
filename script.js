f942512b179740b0aa653821634fefc8ece9a934
DustUnderBag <usamary55@gmail.com>
2024-07-19 17:21:28 -0400

    Add book deletion, fix bug creating existing cards
    Delete feature:
    Delete button is hooked to its corresponding book items via data-index
    with which the delete function targets the book then removes it from the
    library[].
    
    Fix creating the same card.
    Instead of creating card from the newly created book only, the function
    now loops through the library[] to create all cards from scratch.
    
    Refactor

--- script.js
+++ script.js
@@ -11,7 +11,8 @@ const input_isRead = document.querySelector("input#isRead");
 const btn_submit = document.querySelector("button[type='submit']");
 
 //Cards
-const cards_container = document.querySelector(".cards-container");
+const cards_container = document.querySelector('.cards-container');
+
 
 const myLibrary = [];
 
@@ -28,34 +29,14 @@ Book.prototype.getInfo = function() {
     return `${this.title} by ${this.author} has ${this.pages} pages. It ${readState}.`;
 }
 
-function addBookToLibrary(obj) {
-    myLibrary.push(obj);
+Book.prototype.addBookToLibrary = function() {
+    myLibrary.push(this);
+    console.log("Book: " + this.title);
 }
-
-function displayLibrary() {
-    for(let i = 0; i < myLibrary.length; i++) {
-        let id = 'book' + (i + 1);
-        console.log(id);
-        const title = document.querySelector(`#${id} .title`);
-        const author = document.querySelector(`#${id} .author`);
-        const pages = document.querySelector(`#${id} .pages`);
-        console.log(title, author, pages);
-
-        title.textContent = myLibrary[i].title;
-        author.textContent = myLibrary[i].author;
-        pages.textContent = myLibrary[i].pages + " total pages";
-    }
-}
-
-/*
-let book1 = new Book("Pride and Prejudice", "Jane Austen", 363, true);
-let book2 = new Book("The Little Prince", "Antoine de Saint-Exupéry", 102, false);
-*/
 
 btn_newBook.addEventListener('click', () => {
     modal.showModal();
 });
-
 
 function createNewBook() {
     let title = input_title.value;
@@ -64,7 +45,6 @@ function createNewBook() {
     let isRead = (input_isRead.checked) ? true : false;
     
     let temp_obj = new Book(title, author, pages, isRead);
-    console.log(temp_obj);
     return temp_obj;
 }
 
@@ -75,45 +55,83 @@ function clearFormInputs() {
     input_isRead.checked = false;
 }
 
-function createCard(book, index) {
-    const card_DOM = document.createElement('div');
-    card_DOM.classList.add('card');
-    card_DOM.setAttribute('data-index', index);
-
-    const title_DOM = document.createElement('h2');
-    title_DOM.textContent = book.title;
-
-    const author_DOM = document.createElement('p');
-    author_DOM.textContent = book.author;
-
-    const pages_DOM = document.createElement('p');
-    pages_DOM.textContent = book.pages;
-
-    const read_DOM = document.createElement('input');
-    read_DOM.setAttribute('type', 'checkbox');
-    read_DOM.setAttribute('data-index', index);
-    read_DOM.checked = (book.isRead) ? true : false;
-
-    const delete_Btn = document.createElement('button');
-    delete_Btn.textContent = "X";
-    delete_Btn.classList.add('delete_btn');
-    delete_Btn.setAttribute('data-index', index);
-
-    cards_container.appendChild(card_DOM);
-    card_DOM.appendChild(title_DOM);
-    card_DOM.appendChild(author_DOM);
-    card_DOM.appendChild(pages_DOM);
-    card_DOM.appendChild(read_DOM);
-    card_DOM.appendChild(delete_Btn);
+function createCardsFromLibrary(array) {
+    array.forEach( (book, index) => {
+        const card_DOM = document.createElement('div');
+        card_DOM.classList.add('card');
+        card_DOM.setAttribute('data-index', index);
+    
+        const title_DOM = document.createElement('h2');
+        title_DOM.textContent = book.title;
+    
+        const author_DOM = document.createElement('p');
+        author_DOM.textContent = book.author;
+    
+        const pages_DOM = document.createElement('p');
+        pages_DOM.textContent = book.pages;
+    
+        const read_DOM = document.createElement('input');
+        read_DOM.setAttribute('type', 'checkbox');
+        read_DOM.setAttribute('data-index', index);
+        read_DOM.checked = (book.isRead) ? true : false;
+    
+        const delete_Btn = document.createElement('button');
+        delete_Btn.textContent = "X";
+        delete_Btn.classList.add('delete_btn');
+        delete_Btn.setAttribute('data-index', index);
+        delete_Btn.addEventListener('click', deleteBook);
+    
+        cards_container.appendChild(card_DOM);
+        card_DOM.appendChild(title_DOM);
+        card_DOM.appendChild(author_DOM);
+        card_DOM.appendChild(pages_DOM);
+        card_DOM.appendChild(read_DOM);
+        card_DOM.appendChild(delete_Btn);
+    } );
 }
 
 btn_submit.addEventListener('click', e => {
-    addBookToLibrary(createNewBook());
-    let index = myLibrary.length - 1;
-    createCard(myLibrary[index], index);
+    clearCardContainer(); //Remove all cards
 
+    createNewBook().addBookToLibrary(); //Create book obj then store in the library[]
 
-    clearFormInputs();
+    createCardsFromLibrary(myLibrary); //Create cards from scratch.
+    
+    clearFormInputs(); 
     modal.close();
-    e.preventDefault();
+    
+    e.preventDefault(); //prevent submitting the form to server.
 })
+
+
+function deleteBook() {
+    clearCardContainer(); //Remove all cards
+    
+    let index = this.getAttribute('data-index'); //Locate book index inside myLibrary[]
+    deleteBookFromLibrary(index);
+    
+    createCardsFromLibrary(myLibrary); ////Create cards from scratch.
+}
+
+function deleteBookFromLibrary(index) {
+    console.log(`Deleted \"${myLibrary[index].title}\", its index: ${index}`);
+    myLibrary.splice(index, 1);
+}
+
+function clearCardContainer() {
+    while(cards_container.firstElementChild) {
+        cards_container.removeChild(cards_container.lastElementChild);
+    }
+}
+
+
+// Default Books
+let book1 = new Book("Pride and Prejudice", "Jane Austen", 363, true);
+let book2 = new Book("The Little Prince", "Antoine de Saint-Exupéry", 102, false);
+let book3 = new Book("To Kill a Mockingbird", "Harper Lee", 281, true);
+let book4 = new Book("The Handmaid's Tale", "Margaret Atwood", 370, false);
+book1.addBookToLibrary();
+book2.addBookToLibrary();
+book3.addBookToLibrary();
+book4.addBookToLibrary();
+createCardsFromLibrary(myLibrary);
