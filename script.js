b2cffe7f70cd6e30da7d240703f7466db029663d
DustUnderBag <usamary55@gmail.com>
2024-07-30 02:44:54 -0400

    Create style for library cards and its buttons

--- script.js
+++ script.js
@@ -80,20 +80,29 @@ function createCardsFromLibrary(array) {
         const card_DOM = document.createElement('div');
         card_DOM.classList.add('card');
         card_DOM.setAttribute('data-identifier', identifier);
+
+        const bookInfo_DOM = document.createElement('div');
+        bookInfo_DOM.classList.add('book-info');
     
         const title_DOM = document.createElement('h2');
-        title_DOM.textContent = book.title + `id: ${identifier}`;
+        title_DOM.textContent = book.title;
+        title_DOM.classList.add('card-title');
     
         const author_DOM = document.createElement('p');
         author_DOM.textContent = book.author;
+        author_DOM.classList.add('card-author');
     
         const pages_DOM = document.createElement('p');
-        pages_DOM.textContent = book.pages;
+        pages_DOM.textContent = book.pages + ' pages';
+        pages_DOM.classList.add('card-pages');
     
         //To create dropdown menu for reading progress in cards
         const progress_DOM = document.createElement("select");
         progress_DOM.setAttribute("data-identifier", identifier);
+        progress_DOM.classList.add('card-progress')
 
+
+        //Create option elements for all progress states.
         const unread_opt = document.createElement("option");
         unread_opt.setAttribute("value", "unread");
         unread_opt.append("Not started");
@@ -114,16 +123,19 @@ function createCardsFromLibrary(array) {
     
         //Delete button
         const delete_Btn = document.createElement('button');
-        delete_Btn.textContent = "X";
-        delete_Btn.classList.add('delete_btn');
+        delete_Btn.classList.add('delete-btn');
         delete_Btn.setAttribute('data-identifier', identifier);
         delete_Btn.addEventListener('click', deleteBook);
     
         //append elements to card
-        cards_container.appendChild(card_DOM);
-        card_DOM.appendChild(title_DOM);
-        card_DOM.appendChild(author_DOM);
-        card_DOM.appendChild(pages_DOM);
+        cards_container.appendChild(card_DOM); //card as container's child.
+
+        card_DOM.appendChild(bookInfo_DOM); //contentWrapper as card's child.
+        //append all contents to contentWrapper.
+        bookInfo_DOM.appendChild(title_DOM);
+        bookInfo_DOM.appendChild(author_DOM);
+        bookInfo_DOM.appendChild(pages_DOM);
+
         card_DOM.appendChild(progress_DOM);
         card_DOM.appendChild(delete_Btn);
     } );
