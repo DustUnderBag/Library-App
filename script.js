e0e4b8718b81ec77d96afd750363aa3541affc0e
DustUnderBag <usamary55@gmail.com>
2024-10-18 01:41:39 -0400

    Rewrite book constructor into class

--- script.js
+++ script.js
@@ -43,6 +43,7 @@ const myLibrary = [];
 let filtered = [];
 let sorted = [];
 
+/*
 function Book(title, author, pages, progress, rating) {
     this.title = title;
     this.author = author;
@@ -70,6 +71,38 @@ Book.prototype.addBookToLibrary = function() {
     myLibrary.push(this);
     console.log("Book: " + this.title);
 }
+*/
+
+class Book {
+    constructor(title, author, pages, progress, rating){
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.progress = progress;
+        this.rating = rating;
+
+        this.timeAdded = Math.floor( Date.now() / 1000 );
+        this.identifier = Math.floor( Math.random() * 100000 ); // Generate random unique Idex.
+    }
+
+    getInfo() {
+        let progress_str;
+        if(this.progress == "unread") {
+            progress_str = "hasn't started";
+        }else if(this.progress == "reading") {
+            progress_str = "is in progress";
+        }else {
+            progress_str = "is finished";
+        }
+        return `${this.title} by ${this.author} has ${this.pages} pages. Reading ${progress_str}.`;
+    }
+
+    addBookToLibrary() {
+        myLibrary.push(this);
+        console.log("Book: " + this.title);
+    }
+}
+
 
 btn_newBook.addEventListener('click', () => {
     modal.showModal();
